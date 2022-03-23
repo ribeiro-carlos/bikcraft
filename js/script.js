cfdd63cbd8c31404c82587c96716e40e4ec29293
ribeiro-carlos <carlinhos.ars@gmail.com>
2022-03-23 20:03:26 -0300

    parametros url

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//Ativar links Menu
+const links = document.querySelectorAll(".header-menu a");
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+    console.log("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
